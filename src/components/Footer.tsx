6baa80b8f57c1838439e8bb5403a0ba5083fd48c
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-02-10 00:24:56 +0530

    minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,13 +11,13 @@ export default function Footer() {
         <p className="font-playfair italic text-8xl tracking-tighter">
           Collaborate
         </p>
-        <Link href="/contact">
+        <Link href="/customs">
           <button className="bg-black/85 text-white font-montserrat font-bold text-xs mt-4 ml-2 px-8 py-2 uppercase">
             Book a call
           </button>
         </Link>
       </div>
-      <div className="flex flex-row items-start justify-evenly gap-40 uppercase font-montserrat text-xs">
+      <div className="flex flex-row items-start justify-evenly gap-40 uppercase font-montserrat text-sm">
         <div className="flex flex-col gap-6">
           <h1 className="font-bold">Services</h1>
           <Link href="/">Service 1</Link>
