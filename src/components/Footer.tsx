806d295c6dd3c86a490bba8e9e68a7331b5cef0a
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-02-19 06:51:17 +0530

    responsive header and footer for md and lg devices

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,23 +3,23 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="h-[50dvh] flex flex-row items-center justify-evenly my-10 mx-2">
+    <div className="h-[40dvh] flex flex-row items-center justify-evenly my-[2%] mx-[2%]">
       <div>
-        <p className="font-playfair italic text-8xl tracking-tighter ml-4">
+        <p className="font-playfair italic md:text-6xl lg:text-8xl tracking-tighter md:ml-2 lg:ml-4">
           Let's us
         </p>
-        <p className="font-playfair italic text-8xl tracking-tighter">
+        <p className="font-playfair italic md:text-6xl lg:text-8xl tracking-tighter">
           Collaborate
         </p>
         <Link href="/customs">
-          <button className="bg-black/85 text-white font-montserrat font-bold text-xs mt-4 ml-2 px-8 py-2 uppercase">
+          <button className="bg-black/85 text-white font-montserrat font-bold md:text-[10px] lg:text-xs mt-4 md:ml-1 lg:ml-2 md:px-4 lg:px-8 py-2 uppercase">
             Book a call
           </button>
         </Link>
       </div>
-      <div className="flex flex-row items-start justify-evenly gap-40 uppercase font-montserrat text-xs">
+      <div className="flex flex-row items-start justify-evenly md:gap-20 lg:gap-40 uppercase font-montserrat md:text-[8px] lg:text-[10px]">
         <div className="flex flex-col gap-4">
-          <h1 className="font-bold text-sm">Quick Links</h1>
+          <h1 className="font-bold md:text-xs lg:text-sm">Quick Links</h1>
           <Link href="/">Home</Link>
           <Link href="/about">About Us</Link>
           <Link href="/lookbook">Lookbooks</Link>
@@ -28,14 +28,14 @@ export default function Footer() {
           <Link href="/customs">Customs</Link>
         </div>
         <div className="flex flex-col gap-6">
-          <h1 className="font-bold text-sm">Socials</h1>
+          <h1 className="font-bold md:text-xs lg:text-sm">Socials</h1>
           <Link href="/">Instagram</Link>
           <Link href="/">Social 2</Link>
           <Link href="/">Social 3</Link>
           <Link href="/">Social 4</Link>
         </div>
         <div className="flex flex-col gap-6">
-          <h1 className="font-bold text-sm">Transparency</h1>
+          <h1 className="font-bold md:text-xs lg:text-sm">Transparency</h1>
           <Link href="/">Pricing</Link>
           <Link href="/">FAQs</Link>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,40 +2,38 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="fixed top-8 left-[3%] w-[94%] h-[8%] flex items-center bg-quinary/50 backdrop-blur-xl shadow-lg rounded-lg px-8 z-[999]">
+    <div className="hidden fixed top-[3%] left-[3%] w-[94%] h-[8%] md:flex items-center bg-quinary/50 backdrop-blur-xl shadow-lg rounded-lg md:px-4 lg:px-8 z-[999]">
       <Link href="/" className="flex items-center">
         <img
           src="/Header.png"
           alt="Logo"
-          width={200}
-          height={80}
-          className="object-contain"
+          className="lg:w-[200px] md:w-[175px] object-contain"
         />
       </Link>
 
       {/* Navigation Links */}
-      <nav className="absolute left-1/2 transform -translate-x-1/2 flex items-center gap-8 uppercase text-2xl font-montserrat">
+      <nav className="absolute left-1/2 transform -translate-x-1/2 flex items-center lg:gap-8 md:gap-4 uppercase lg:text-xl md:text-sm font-montserrat">
         <Link
           href="/about"
-          className="text-white hover:text-white/70 duration-100 transition-colors"
+          className="text-white hover:text-white/70 duration-150 transition-colors"
         >
           About Us
         </Link>
         <Link
           href="/lookbook"
-          className="text-white hover:text-white/70 duration-100 transition-colors"
+          className="text-white hover:text-white/70 duration-150 transition-colors"
         >
           Lookbook
         </Link>
         <Link
           href="/blogs"
-          className="text-white hover:text-white/70 duration-100 transition-colors"
+          className="text-white hover:text-white/70 duration-150 transition-colors"
         >
           Blogs
         </Link>
         <Link
           href="/customs"
-          className="text-white hover:text-white/70 duration-100 transition-colors"
+          className="text-white hover:text-white/70 duration-150 transition-colors"
         >
           Customs
         </Link>
@@ -45,7 +43,7 @@ export default function Header() {
       <div className="ml-auto">
         <Link
           href="https://shop.labeljuilakhani.com"
-          className="text-lg font-montserrat font-bold uppercase text-white border border-white px-6 py-2 rounded-full hover:underline"
+          className="md:text-sm lg:text-lg font-montserrat font-bold uppercase text-white border border-white md:px-4 md:py-2 lg:px-6 lg:py-2 rounded-full hover:underline"
         >
           Shop now
         </Link>
