90d7160bddca32eb7dc658fc9fbd6bca5455e783
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-02-19 10:45:08 +0530

    responsive footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,23 +3,25 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="h-[40dvh] flex flex-row items-center justify-evenly my-[2%] mx-[2%]">
-      <div>
-        <p className="font-playfair italic md:text-6xl lg:text-8xl tracking-tighter md:ml-2 lg:ml-4">
-          Let's us
-        </p>
-        <p className="font-playfair italic md:text-6xl lg:text-8xl tracking-tighter">
-          Collaborate
-        </p>
+    <div className="h-[45dvh] md:h-[40dvh] flex flex-col md:flex-row items-center justify-center md:justify-evenly my-[2%] mx-[2%] gap-8 md:gap-0 bg-black/10 md:bg-inherit">
+      <div className="flex flex-row md:flex-col items-center md:items-start gap-8 md:gap-0">
+        <div className="font-playfair italic text-5xl md:text-6xl lg:text-8xl tracking-tighter">
+          <p className="ml-2 lg:ml-4">
+            Let's us
+          </p>
+          <p className="">
+            Collaborate
+          </p>
+        </div>
         <Link href="/customs">
-          <button className="bg-black/85 text-white font-montserrat font-bold md:text-[10px] lg:text-xs mt-4 md:ml-1 lg:ml-2 md:px-4 lg:px-8 py-2 uppercase">
+          <button className="bg-black/85 text-white font-montserrat font-bold text-xs md:text-[10px] lg:text-xs mt-4 md:ml-1 lg:ml-2 px-4 lg:px-8 py-2 uppercase">
             Book a call
           </button>
         </Link>
       </div>
-      <div className="flex flex-row items-start justify-evenly md:gap-20 lg:gap-40 uppercase font-montserrat md:text-[8px] lg:text-[10px]">
-        <div className="flex flex-col gap-4">
-          <h1 className="font-bold md:text-xs lg:text-sm">Quick Links</h1>
+      <div className="flex flex-row items-start justify-center md:justify-evenly gap-10 md:gap-20 lg:gap-40 uppercase font-montserrat text-[10px] md:text-[8px] lg:text-[10px]">
+        <div className="flex flex-col gap-3 md:gap-4">
+          <h1 className="font-bold text-xs lg:text-sm">Quick Links</h1>
           <Link href="/">Home</Link>
           <Link href="/about">About Us</Link>
           <Link href="/lookbook">Lookbooks</Link>
@@ -27,15 +29,15 @@ export default function Footer() {
           <Link href="/blogs">Blogs</Link>
           <Link href="/customs">Customs</Link>
         </div>
-        <div className="flex flex-col gap-6">
-          <h1 className="font-bold md:text-xs lg:text-sm">Socials</h1>
+        <div className="flex flex-col gap-4 md:gap-6">
+          <h1 className="font-bold text-xs lg:text-sm">Socials</h1>
           <Link href="/">Instagram</Link>
           <Link href="/">Social 2</Link>
           <Link href="/">Social 3</Link>
           <Link href="/">Social 4</Link>
         </div>
-        <div className="flex flex-col gap-6">
-          <h1 className="font-bold md:text-xs lg:text-sm">Transparency</h1>
+        <div className="flex flex-col gap-4 md:gap-6">
+          <h1 className="font-bold text-xs lg:text-sm">Transparency</h1>
           <Link href="/">Pricing</Link>
           <Link href="/">FAQs</Link>
         </div>
