e49797213050209bc2a173b5d004763ef0a70aac
Siddhesh Lakhani <lakhani.siddhesh@gmail.com>
2025-02-27 20:47:27 +0530

    Footer changed and font sizes updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,43 +3,78 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <div className="h-[45dvh] md:h-[40dvh] flex flex-col md:flex-row items-center justify-center md:justify-evenly gap-8 md:gap-0 bg-primary/10 md:bg-inherit m-2">
-      <div className="flex flex-row md:flex-col items-center md:items-start gap-8 md:gap-0">
-        <div className="font-playfair italic text-4xl md:text-6xl lg:text-8xl tracking-tighter">
-          <p className="ml-2 lg:ml-4">
-            Let's
-          </p>
-          <p className="">
-            Collaborate
-          </p>
-        </div>
+    <div className="h-[45dvh] md:h-[40dvh] flex flex-col items-center justify-center md:justify-evenly gap-8 md:gap-8 lg:gap-12 mb-2 md:mb-4 lg:mb-8">
+      <div className="flex flex-col md:flex-row items-center justify-evenly w-full bg-primary px-6 md:px-12 lg:px-20 py-6 md:py-8 gap-4 md:gap-0">
+        <p className="font-playfair italic text-3xl md:text-5xl lg:text-6xl tracking-tight text-quaternary">
+          Let's Collaborate!
+        </p>
         <Link href="/customs">
-          <button className="bg-black text-quaternary font-montserrat font-bold text-xs md:text-[10px] lg:text-xs mt-4 md:ml-1 lg:ml-2 px-4 lg:px-8 py-2 uppercase">
-            Book a call
+          <button className="bg-quaternary text-primary font-montserrat font-bold text-xs md:text-sm lg:text-base px-4 md:px-6 lg:px-8 py-2 uppercase shadow-md">
+            Book a Call
           </button>
         </Link>
       </div>
-      <div className="flex flex-row items-start justify-center md:justify-evenly gap-6 md:gap-20 lg:gap-40 uppercase font-montserrat text-[10px] md:text-[8px] lg:text-[10px]">
+
+      <div className="flex flex-row items-start justify-center md:justify-evenly gap-8 md:gap-20 lg:gap-40 uppercase font-montserrat text-[8px] md:text-[8px] lg:text-[10px]">
         <div className="flex flex-col gap-3 md:gap-4">
           <h1 className="font-bold text-[10px] lg:text-sm">Quick Links</h1>
-          <Link className="hover:underline" href="/">Home</Link>
-          <Link className="hover:underline" href="/lookbook">Lookbooks</Link>
-          <Link className="hover:underline" href="/shop">Shop</Link>
-          <Link className="hover:underline" href="/customs">Customs</Link>
-          <Link className="hover:underline" href="/blogs">Blogs</Link>
-          <Link className="hover:underline" href="/about">About Us</Link>
+          <Link className="hover:underline" href="/">
+            Home
+          </Link>
+          <Link className="hover:underline" href="/lookbook">
+            Lookbooks
+          </Link>
+          <Link className="hover:underline" href="/shop">
+            Shop
+          </Link>
+          <Link className="hover:underline" href="/customs">
+            Customs
+          </Link>
+          <Link className="hover:underline" href="/blogs">
+            Blogs
+          </Link>
+          <Link className="hover:underline" href="/about">
+            About Us
+          </Link>
         </div>
         <div className="flex flex-col gap-4 md:gap-6">
           <h1 className="font-bold text-[10px] lg:text-sm">Socials</h1>
-          <Link className="hover:underline" href="https://instagram.com/labeljuilakhani" target="_blank">Instagram</Link>
-          <Link className="hover:underline" href="https://www.linkedin.com/in/jui-lakhani-604030226/" target="_blank">LinkedIn</Link>
-          <Link className="hover:underline" href="https://www.facebook.com/profile.php?id=61573638958217" target="_blank">Facebook</Link>
+          <Link
+            className="hover:underline"
+            href="https://instagram.com/labeljuilakhani"
+            target="_blank"
+          >
+            Instagram
+          </Link>
+          <Link
+            className="hover:underline"
+            href="https://www.linkedin.com/in/jui-lakhani-604030226/"
+            target="_blank"
+          >
+            LinkedIn
+          </Link>
+          <Link
+            className="hover:underline"
+            href="https://www.facebook.com/profile.php?id=61573638958217"
+            target="_blank"
+          >
+            Facebook
+          </Link>
         </div>
         <div className="flex flex-col gap-4 md:gap-6">
           <h1 className="font-bold text-[10px] lg:text-sm">Transparency</h1>
-          <Link className="hover:underline" href="/pages/terms-and-conditions">Terms and Conditions</Link>
-          <Link className="hover:underline" href="/pages/frequently-asked-questions">FAQs</Link>
-          <Link className="hover:underline" href="/pages/shipping-and-payments">Shipping and Payments</Link>
+          <Link className="hover:underline" href="/pages/terms-and-conditions">
+            Terms and Conditions
+          </Link>
+          <Link
+            className="hover:underline"
+            href="/pages/frequently-asked-questions"
+          >
+            FAQs
+          </Link>
+          <Link className="hover:underline" href="/pages/shipping-and-payments">
+            Shipping and Payments
+          </Link>
         </div>
       </div>
     </div>
--- src/components/HomePage/AboutSection.tsx
+++ src/components/HomePage/AboutSection.tsx
@@ -6,13 +6,13 @@ export default function AboutSection() {
       <p className="text-xs md:text-sm uppercase tracking-[0.5em] font-montserrat font-medium text-secondary">
         About us
       </p>
-      <div className="text-[10vw] md:text-7xl lg:text-8xl font-playfair text-secondary tracking-tighter mt-2">
+      <div className="text-[10vw] md:text-7xl lg:text-[5vw] font-playfair text-secondary tracking-tighter mt-2">
         <p className="leading-[0.9]">More than</p>
         <p className="uppercase text-primary leading-[0.9]">
-          Clothing <span className="text-[9vw] md:text-6xl lg:text-7xl lowercase text-secondary italic">we</span>
+          Clothing <span className="text-[9vw] md:text-6xl lg:text-[4vw] lowercase text-secondary italic">we</span>
         </p>
         <p className="uppercase leading-[0.9]">
-          <span className="text-[9vw] md:text-6xl lg:text-7xl lowercase italic">create</span> <span className=""> Statements </span>
+          <span className="text-[9vw] md:text-6xl lg:text-[4vw] lowercase italic">create</span> <span className=""> Statements </span>
         </p>
       </div>
 
@@ -20,7 +20,7 @@ export default function AboutSection() {
         Welcome to the world of Jui Lakhani, where every creation is crafted
         with love and designed to be uniquely yours!
       </h1>
-      <p className="font-sen text-xs md:text-base lg:text-lg mt-6 text-primary/60 tracking-tight">
+      <p className="font-sen text-xs md:text-base lg:text-[0.9vw] mt-6 text-primary/60 tracking-tight">
         Our Label is built on a passion for fashion that is both creative and
         conscious. Whether you are looking for everyday essentials or a
         statement piece, we have something that speaks to your style. We are
